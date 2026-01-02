4f57f662579f4d81a71c834d5ae2b5d163e9a96c
dchizhikov <dchizhikov@gmail.com>
2026-01-02 13:52:41 +0000

    Fix html_templates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
-from html import login_page
+from html_templates import login_page
 
 app = FastAPI()
 
@@ -10,10 +10,10 @@ def root():
 def health():
     return {"status": "healthy"}
 
-def echo(data):
+async def echo(data):
     return JSONResponse({"echo": data})
 
 #app.get("/")(root)
 app.get("/", response_class=HTMLResponse)(lambda: login_page)
-app.get("/health")(health)
-app.post("/echo")(echo)
+#app.get("/health")(health)
+#app.post("/echo")(echo)
